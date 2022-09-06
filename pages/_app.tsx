67d5f1fd8d7f6d85a0cb9b4cc16215c217c9d48d
mpatel500 <mpatel125@outlook.com>
2022-09-06 21:03:35 +0100

    Get and update inventory at top-level component to persist through navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
+import React, { useEffect, useState } from 'react';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { getInventory, defaultInventory } from './data/inventory'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [inventory, setInventory] = useState(defaultInventory);
+
+  useEffect(() => {
+    const defaultInventory = getInventory();
+    setInventory(defaultInventory);
+  }, []);
+
+  const handleSeedChange = (key: string, value: string | number) => {
+    const seeds = {...inventory['seeds'], [key]: value}
+    setInventory({ ...inventory, seeds} );
+  }
+  
+  return (
+    <Component
+      {...pageProps}
+      handleSeedChange={handleSeedChange}
+      inventory={inventory}
+    />
+  );
 }
 
 export default MyApp
